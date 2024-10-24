70ced9af350e22c3e9df4e2d141073a59d6c2df2
Felix <felix.jontegen@th1ng.se>
2024-10-24 14:51:36 +0200

    removed extra id paramter

--- src/lynx/controller/installation_controller.py
+++ src/lynx/controller/installation_controller.py
@@ -37,27 +37,27 @@ class InstallationController(Controller):
         resp = self._api_client.request("DELETE", path)
         return OKResponse(**resp)
 
-    def get_installation_meta(self, installation_id: int, id: int, key: str):
-        path = "api/v2/installation/{0}/{1}/meta/{2}".format(installation_id, id, key)
+    def get_installation_meta(self, installation_id: int, key: str):
+        path = "api/v2/installation/{0}/meta/{1}".format(installation_id, key)
         resp = self._api_client.request("GET", path)
         return MetaObject(**resp)
 
-    def create_installation_meta(self, installation_id: int, id: int, key: str, meta: MetaObject,
+    def create_installation_meta(self, installation_id: int, key: str, meta: MetaObject,
                                  silent: Optional[bool] = False):
         qs = self.format_query_string(dict({"silent": silent}))
-        path = "api/v2/installation/{0}/{1}/meta/{2}{3}".format(installation_id, id, key, qs)
+        path = "api/v2/installation/{0}/meta/{1}{2}".format(installation_id, key, qs)
         resp = self._api_client.request("POST", path, json=meta.to_dict())
         return MetaObject(**resp)
 
-    def update_installation_meta(self, installation_id: int, id: int, key: str, meta: MetaObject,
+    def update_installation_meta(self, installation_id: int, key: str, meta: MetaObject,
                                  silent: Optional[bool] = False, create_missing: Optional[bool] = False):
         qs = self.format_query_string(dict({"silent": silent, "create_missing": create_missing}))
-        path = "api/v2/installation/{0}/{1}/meta/{2}{3}".format(installation_id, id, key, qs)
+        path = "api/v2/installation/{0}/meta/{1}{2}".format(installation_id, key, qs)
         resp = self._api_client.request("PUT", path, json=meta.to_dict())
         return MetaObject(**resp)
 
-    def delete_installation_meta(self, installation_id: int, id: int, key: str, silent: Optional[bool] = False):
+    def delete_installation_meta(self, installation_id: int, key: str, silent: Optional[bool] = False):
         qs = self.format_query_string(dict({"silent": silent}))
-        path = "api/v2/installation/{0}/{1}/meta/{2}{3}".format(installation_id, id, key, qs)
+        path = "api/v2/installation/{0}/meta/{1}{2}".format(installation_id, key, qs)
         resp = self._api_client.request("DELETE", path)
         return OKResponse(**resp)
